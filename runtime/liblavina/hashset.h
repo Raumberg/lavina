81cc1ab5050c757770c4822b41b7632398fb3c79
Raumberg <yashestik@gmail.com>
2026-02-15 21:35:50 +0300

    std: added collections.lv, added new functions in hashset.h, vector.h

--- runtime/liblavina/hashset.h
+++ runtime/liblavina/hashset.h
@@ -10,3 +10,33 @@ template<typename T>
 void lv_remove(std::unordered_set<T>& s, const T& val) {
     s.erase(val);
 }
+
+// --- collections module functions ---
+
+// Set union
+template<typename T>
+std::unordered_set<T> __lv_col_set_union(const std::unordered_set<T>& a, const std::unordered_set<T>& b) {
+    std::unordered_set<T> result = a;
+    for (const auto& x : b) result.insert(x);
+    return result;
+}
+
+// Set intersection
+template<typename T>
+std::unordered_set<T> __lv_col_set_intersection(const std::unordered_set<T>& a, const std::unordered_set<T>& b) {
+    std::unordered_set<T> result;
+    for (const auto& x : a) {
+        if (b.count(x) > 0) result.insert(x);
+    }
+    return result;
+}
+
+// Set difference (a - b)
+template<typename T>
+std::unordered_set<T> __lv_col_set_difference(const std::unordered_set<T>& a, const std::unordered_set<T>& b) {
+    std::unordered_set<T> result;
+    for (const auto& x : a) {
+        if (b.count(x) == 0) result.insert(x);
+    }
+    return result;
+}
